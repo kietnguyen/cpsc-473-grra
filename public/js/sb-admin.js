aac20589fe3449497b9250d9c4eb837cf3e7503c
Kiet Nguyen <kietnguyen@csu.fullerton.edu>
2014-04-12 02:52:32 -0700

    more styling for fun

--- public/js/sb-admin.js
+++ public/js/sb-admin.js
@@ -1,18 +1,20 @@
 $(function() {
-
-    $('#side-menu').metisMenu();
-
+  $("#side-menu").metisMenu();
 });
 
 //Loads the correct sidebar on window load,
 //collapses the sidebar on window resize.
 $(function() {
-    $(window).bind("load resize", function() {
-        console.log($(this).width())
-        if ($(this).width() < 768) {
-            $('div.sidebar-collapse').addClass('collapse')
-        } else {
-            $('div.sidebar-collapse').removeClass('collapse')
-        }
-    })
-})
+  $(window).bind("load resize", function() {
+    console.log($(this).width());
+    if ($(this).width() < 768) {
+      $("div.sidebar-collapse").addClass("collapse");
+      $(".sidebar-custom").css("width", "100%");
+      $(".sidebar-custom").css("background-color", "inherit");
+    } else {
+      $("div.sidebar-collapse").removeClass("collapse");
+      $(".sidebar-custom").css("width", "inherit");
+      $(".sidebar-custom").css("height", "auto");
+    }
+  });
+});
